c9adeace12bc339def12fc3b998c6c896a117491
Josh Demers <156728601+Shhhua1@users.noreply.github.com>
2025-04-20 15:07:38 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Read the data from the CSV file
+df = pd.read_csv('rentfaster_cleaned.csv')
+
+print(df)
